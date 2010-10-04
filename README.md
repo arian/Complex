f868f8ad116780c5aef12d7032bab2a611758517
Arian <arian@aryweb.nl>
2010-10-04 19:59:30 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Complex
+=======
+
+Complex is a additional Type to deal with Complex Numbers in JavaScript. It provides several methods to add, multiply numbers as well as
+calculate the magnitude and angle in the complex plane.
+
+
+Build
+-----
+
+Build via [Packager](http://github.com/kamicane/packager), requires MooTools Core to be registered to Packager already _(ty cpojer)_
+
+	./packager register /path/to/Complex
+	./packager build Complex/* > Complex
+
---
+++ Source/Number.js
@@ -0,0 +1,48 @@
+/*
+---
+description: Provides a way to use Complex Numbers in JavaScript
+
+license: MIT-style
+
+authors:
+  - Arian Stolwijk
+
+requires: [Core/Number, Complex]
+
+provides:
+  - Number
+
+...
+*/
+
+Number.precision = 1e-6;
+
+(function(){
+
+var methods = {};
+['multiply', 'devide', 'add', 'substract', 'power'].each(function(method){
+	methods[method] = function(number){
+		return Complex.from(this)[method](number);
+	};
+});
+
+Object.append(methods, {
+	toComplex: function(){
+		return Complex.from(this);
+	}
+});
+
+Number.implement(methods);
+
+})();
+
+// Replace sqrt method so it can handle negative values
+(function(sqrt){
+
+Number.prototype.sqrt = function(){
+	if(this < 0) return new Complex(0, Math.sqrt(-this));
+	return sqrt.apply(this);
+};
+
+})(Number.prototype.sqrt);
+
